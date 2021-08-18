61ac9b0505236942036e0407329c2221e5924da5
durgeshkadagala <89067446+durgeshkadagala@users.noreply.github.com>
2021-08-18 08:34:25 -0400

    react js assignment day 1

---
+++ react-demo/src/Banner.js
@@ -0,0 +1,8 @@
+function Banner(){
+    return(
+        <h1>This is My Assignment</h1>
+    )
+}
+
+
+export default Banner;
---
+++ react-demo/src/Header.js
@@ -0,0 +1,13 @@
+function Header(){
+    let name= "Durgesh";
+    return(
+
+        
+        <h1>Hello This is {name}</h1>
+        
+        
+    )
+}
+
+
+export default Header;
